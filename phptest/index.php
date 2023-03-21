871dbeb21abf73fddab88fea046dd7b8121544f7
discoveryX64 <119447512+discoveryX64@users.noreply.github.com>
2023-03-21 16:32:42 +0000

    added php test

---
+++ phptest/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"> <!-- Bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js"></script> <!-- Bootstrap JS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"> <!-- Bootstrap Icons-->
+        <link rel="icon" type="image/x-icon" href="https://discoveryx64.github.io/gamesgames/images/favicon.ico"> <!-- Favicon -->
+        <title>GamesGames - Home</title>
+        <style>
+            .header {
+                size: 48px;
+                font-size: 48px;
+                color: black;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+            .text {
+                size: 24px;
+                font-size: 24px;
+                color: black;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+            .center {
+                text-align: center;
+                align-content: center;
+                align-items: center;
+            }
+            .bottom {
+                bottom: 0px;
+                position: fixed;
+            }
+        </style>
+    </head>
+    <body>
+        <a href="https://discoveryx64.github.io/gamesgames/"><span class="text">Home</span></a> <i class="bi bi-house-fill"></i>
+        <center>heheha</center>
+        <div class="bottom">
+            <a href="https://github.com/discoveryX64/gamesgames/">Github</a><i class="bi bi-github"></i>
+        </div>
+    </body>
+</html>
